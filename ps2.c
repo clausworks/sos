139c3dafedd0289b40f76161189380d096974202
Nicholas Brunet <clausworks@zoho.com>
2022-04-14 12:46:02 -0700

    Add inb, outb; run PS2 self-test

--- ps2.c
+++ ps2.c
@@ -4,58 +4,89 @@
 #include "vga.h"
 #include "memory.h"
 
-uint8_t *const ps2_command = (uint8_t *)0x64;
-PS2Status *const ps2_status = (PS2Status *)0x64;
-uint8_t *const ps2_data = (uint8_t *)0x60;
+static uint8_t inb(uint16_t port) {
+   uint8_t ret;
+   asm volatile ( "inb %1, %0"
+   : "=a"(ret)
+   : "Nd"(port) );
+   return ret;
+}
 
-void ps2_send_cmd(uint8_t cmd, uint8_t *resp) {
-   printk("ps2_send_cmd: %hx\n", cmd);
-   while (ps2_status->input_full);
-   *ps2_command = cmd;
+static void outb(uint16_t port, uint8_t val) {
+   asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+void ps2_send_cmd(uint8_t cmd) {
+   uint8_t status = inb(PS2_CMD_PORT);
+   printk("ps2_send_cmd: %hx...", cmd);
+   
+   while (PS2_STATUS_INPUT & status) {
+      status = inb(PS2_STATUS_PORT);
+   }
+   outb(PS2_DATA_PORT, cmd); 
+
+   printk("done\n");
+}
+
+uint8_t ps2_read_data() {
+   uint8_t data;
+   uint8_t status = inb(PS2_CMD_PORT);
+   printk("ps2_read_data...");
+   
+   while (!(PS2_STATUS_OUTPUT & status)) {
+      status = inb(PS2_STATUS_PORT);
+   }
+   data = inb(PS2_DATA_PORT);
+
+   printk("done (%hx)\n", data);
+   return data;
 }
 
 void ps2_write_data(uint8_t data) {
-   printk("ps2_write_data: %hx\n", data);
-   while (ps2_status->input_full);
-   *ps2_data = data;
+   uint8_t status = inb(PS2_CMD_PORT);
+   printk("ps2_write_data: %hx...", data);
+
+   while (PS2_STATUS_INPUT & status) {
+      status = inb(PS2_STATUS_PORT);
+   }
+   outb(PS2_DATA_PORT, data); 
+
+   printk("done\n");
 }
 
 void ps2_init() {
    int dual_port;
-   uint8_t response, data;
-   PS2Config *config;
+   uint8_t response, config;
 
    /* Disable ports */
-   ps2_send_cmd(PS2CMD_DISABLE_P1, NULL);
-   ps2_send_cmd(PS2CMD_DISABLE_P2, NULL);
+   ps2_send_cmd(PS2CMD_DISABLE_P1);
+   ps2_send_cmd(PS2CMD_DISABLE_P2);
 
    /* Flush output buffer */
-   while (!ps2_status->output_full);
-   response = *ps2_data;
+   inb(PS2_DATA_PORT);
 
    /* Set config byte */
-   ps2_send_cmd(PS2CMD_GETCONFIG, &response);
+   ps2_send_cmd(PS2CMD_GETCONFIG);
+   config = ps2_read_data();
 
-   data = response; /* make copy */
-   config = (PS2Config *)&data; /* modify copy */
-   config->p1_int_en = 0;
-   config->p2_int_en = 0;
-   config->p1_trans = 0;
-   dual_port = config->p2_clk; /* check whether second port exists */
+   config &= ~(PS2_CONFIG_P1INTEN);
+   config &= ~(PS2_CONFIG_P2INTEN);
+   config &= ~(PS2_CONFIG_TRANS);
+   dual_port = config & PS2_CONFIG_P2CLK; /* check whether second port exists */
    printk("dual_port: %s\n", (dual_port) ? "yes" : "no");
 
-   ps2_send_cmd(PS2CMD_SETCONFIG, &response);
-   ps2_write_data(data);
+   ps2_send_cmd(PS2CMD_SETCONFIG);
+   ps2_write_data(config);
 
    /* Self test */
-   ps2_send_cmd(PS2CMD_SELFTEST, &response);
-   if (response == PS2RESP_ST_PASS) {
+   ps2_send_cmd(PS2CMD_SELFTEST);
+   if ((response = ps2_read_data()) == PS2RESP_ST_PASS) {
       printk("passed self-test\n");
    }
    else if (response == PS2RESP_ST_FAIL) {
       printk("failed self-test\n");
    }
    else {
-      printk("unknown response from self-test\n");
+      printk("unknown response from self-test: %hx\n", response);
    }
 }
--- ps2.h
+++ ps2.h
@@ -1,6 +1,10 @@
 #ifndef PS2H
 #define PS2H
 
+#define PS2_CMD_PORT 0x64
+#define PS2_STATUS_PORT PS2_CMD_PORT
+#define PS2_DATA_PORT 0x60
+
 #define PS2CMD_RCONFIG 0x20
 #define PS2CMD_DISABLE_P1 0xAD
 #define PS2CMD_DISABLE_P2 0xA7
@@ -10,26 +14,13 @@
 #define PS2RESP_ST_FAIL 0xFC
 #define PS2RESP_ST_PASS 0x55
 
-typedef struct PS2Status {
-   uint8_t output_full : 1;
-   uint8_t input_full : 1;
-   uint8_t system_flag : 1;
-   uint8_t command : 1;
-   uint8_t : 2;
-   uint8_t timeout_err : 1;
-   uint8_t parity_err : 1;
-} PS2Status;
+#define PS2_STATUS_OUTPUT 1
+#define PS2_STATUS_INPUT (1 << 1)
 
-typedef struct PS2Config {
-   uint8_t p1_int_en : 1;
-   uint8_t p2_int_en : 1;
-   uint8_t system_flag : 1;
-   uint8_t : 1;
-   uint8_t p1_clk : 1;
-   uint8_t p2_clk : 1;
-   uint8_t p1_trans : 1;
-   uint8_t : 1;
-} PS2Config;
+#define PS2_CONFIG_P1INTEN (1 << 0)
+#define PS2_CONFIG_P2INTEN (1 << 1)
+#define PS2_CONFIG_P2CLK (1 << 5)
+#define PS2_CONFIG_TRANS (1 << 6)
 
 void ps2_init(void);
 
